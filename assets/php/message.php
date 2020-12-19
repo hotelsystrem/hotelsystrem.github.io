03ce85876bcb8a45f628fce13b749acef31c6096
hotelsystrem <75790758+hotelsystrem@users.noreply.github.com>
2020-12-19 09:19:39 +0600

    Add files via upload

---
+++ assets/php/message.php
@@ -0,0 +1,20 @@
+<!-- Через 10 секунд после появления сообщения об отправке или ошибке — отправляемся на сайт Кода :) -->
+<meta http-equiv='refresh' content='10; url=https://static9.depositphotos.com/1431107/1108/i/600/depositphotos_11085415-stock-photo-error-icon.jpg'>
+<meta charset="UTF-8" />
+<!-- Начался блок PHP -->
+<?php
+// Получаем значения переменных из пришедших данных
+$name = $_POST['name'];
+$email = $_POST['email'];
+$header = $_POST['header'];
+$message = $_POST['message'];
+// Формируем сообщение для отправки, в нём мы соберём всё, что ввели в форме
+$mes = "Имя: $name \nE-mail: $email \nТема: $header \nТекст: $message";
+// Пытаемся отправить письмо по заданному адресу
+// Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
+$send = mail($email, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+// Если отправка прошла успешно — так и пишем
+if ($send == 'true') {echo "Сообщение отправлено";}
+// Если письмо не ушло — выводим сообщение об ошибке
+else {echo "Ой, что-то пошло не так";}
+?>
